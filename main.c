c6cee819d7c221a8e26ca9ee1cd4592d0a6f1a07
Ohto Myllynen <ohromy@utu.fi>
2013-10-11 08:11:33 +0300

    Added basic SDL diplay loop to main

--- main.c
+++ main.c
@@ -12,7 +12,19 @@ cleanup()
 int
 main(int argc, char **argv)
 {
+    int run;
+
     printf("Hello raytracer\n");
+
+    gfx_init("Hello raytracer!");
+    atexit(cleanup);
+
+    run = 1;
+    while(1 == run)
+    {
+        getInput();
+        SDL_Delay(16);
+    }
 
     cleanup();
 
