5ef465bf5f22016ad6ed9cf6687f0b0d72d88c1f
Ohto Myllynen <ohromy@utu.fi>
2013-10-11 08:04:02 +0300

    Added cleanup function to main

--- main.c
+++ main.c
@@ -3,9 +3,18 @@
  */
 #include "includes.h"
 
+void
+cleanup()
+{
+    gfx_cleanup();
+}
+
 int
 main(int argc, char **argv)
 {
     printf("Hello raytracer\n");
+
+    cleanup();
+
     return 0;
 }
