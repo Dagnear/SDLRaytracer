9e7b0f632a73c408c8266036e3fa2c7a3a51b27b
Ohto Myllynen <ohromy@utu.fi>
2013-10-11 10:28:13 +0300

    Added structs and enumeration for different elements in raytracer. Should there be a seperate file?

--- defines.h
+++ defines.h
@@ -1,1 +1,36 @@
+/* Variables */
 SDL_Surface *screen;
+
+/* Enumertions */
+enum entity_type
+{
+    Sphere
+};
+
+/* Structures */
+typedef struct
+{
+    int x;
+    int y;
+    int z;
+}; Vector;
+
+typedef struct
+{
+    Vector position;
+    Vector direction;
+}; Ray;
+
+typedef struct
+{
+    Vector position;
+    int radius;
+}; Sphere;
+
+typedef struct
+{
+    int type;
+    void *entity;
+}; Entity;
+
+
