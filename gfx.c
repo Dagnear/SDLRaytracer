ae82a264c3ad85126e6390b3bb0ec72f8c0cfdb1
Ohto Myllynen <ohromy@utu.fi>
2013-10-11 07:31:20 +0300

    Added init and cleanup SDL functions in gfx.c

---
+++ gfx.c
@@ -0,0 +1,28 @@
+/*
+ * SDL functions
+ */
+void
+init(char *title)
+{
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("Could not initialize SDL: %s\n",SDL_GetError());
+        exit(1);
+    }
+
+    screen = SDL_SetVideoMode(640,480,0,SDL_HWPALETTE);
+
+    if(screen == NULL)
+    {
+        printf("Could not set screen mode to 640x480 %s\n",SDL_GetError());
+        exit(1);
+    }
+
+    SDL_WM_SetCaption(title,NULL);
+}
+
+void
+sdl_cleanup()
+{
+    SDL_Quit();
+}
