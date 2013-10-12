3e7f28f87f40b02a4f39bad97f3c97909cc58bca
Ohto Myllynen <ohromy@utu.fi>
2013-10-12 09:56:40 +0300

    Added buffer initializations in gfx_init method

--- gfx.c
+++ gfx.c
@@ -5,27 +5,45 @@
 #include "defines.h"
 
 void
-gfx_init(char *title)
+gfx_init(int width, int height, char *title)
 {
+    /* Initialize SDL */
     if(SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        printf("Could not initialize SDL: %s\n",SDL_GetError());
+        printf("[ERROR] Could not initialize SDL: %s\n",SDL_GetError());
         exit(1);
     }
 
-    screen = SDL_SetVideoMode(640,480,0,SDL_HWPALETTE);
+    /* Initialize window */
+    screen = SDL_SetVideoMode(width,height,0,SDL_HWPALETTE);
 
-    if(screen == NULL)
+    if(NULL == screen)
     {
-        printf("Could not set screen mode to 640x480 %s\n",SDL_GetError());
+        printf("[ERROR] Could not set screen mode to 640x480 %s\n",SDL_GetError());
         exit(1);
     }
 
+    /* Initialize buffer */
+    buffer = SDL_CreateRGBSurface(SDL_HWSURFACE, width, height,
+                screen->format->BitsPerPixel,
+                screen->format->Rmask,
+                screen->format->Gmask,
+                screen->format->Bmask,
+                screen->format->Amask
+            );
+
+    if(NULL == buffer)
+    {
+        printf("[ERROR] Could not initialize buffer %s\n",SDL_GetError());
+    }
+                
     SDL_WM_SetCaption(title,NULL);
 }
 
 void
 gfx_cleanup()
 {
+    SDL_FreeSurface(buffer);
+    SDL_FreeSurface(screen);
     SDL_Quit();
 }
