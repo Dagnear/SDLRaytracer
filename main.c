ea80128aa8408c0bcff7e282d048703c040ceddc
Ohto Myllynen <ohromy@utu.fi>
2013-10-12 16:23:58 +0300

    Added drawing test to main.c

--- main.c
+++ main.c
@@ -20,6 +20,12 @@ main(int argc, char **argv)
     gfx_init(640,480,"Hello raytracer!");
     atexit(cleanup);
 
+    Pixel yellow = gfx_getPixel(255,255,0);
+    gfx_lockBuffer();
+    gfx_putPixel(50,50,yellow);
+    gfx_unlockBuffer();
+    gfx_update();
+
     run = 1;
     while(1 == run)
     {
