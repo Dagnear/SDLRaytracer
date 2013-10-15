acd41cadefa864be97380f6fba10930e3af28d72
Ohto Myllynen <ohromy@utu.fi>
2013-10-15 18:25:40 +0300

    Moved gfx specific declarations to gfx.h

--- declare.h
+++ declare.h
@@ -1,15 +1,6 @@
 #include "gfx.h"
 #include "tracer.h"
 
-void gfx_init(int width, int height, char *title);
-void gfx_cleanup();
-void gfx_putPixel(int x, int y, Uint32 color);
-void gfx_update();
-void gfx_lockBuffer();
-void gfx_unlockBuffer();
-Pixel gfx_getPixel(int r, int g, int b);
-void gfx_fillBuffer(Pixel color);
-
 void rt_initScene();
 void rt_printScene();
 void rt_setCameraPosition();
--- gfx.h
+++ gfx.h
@@ -8,3 +8,13 @@ typedef struct
     int height;
     Pixel *pixels;
 } Bitmap;
+
+void gfx_init(int width, int height, char *title);
+void gfx_cleanup();
+void gfx_putPixel(int x, int y, Uint32 color);
+void gfx_update();
+void gfx_lockBuffer();
+void gfx_unlockBuffer();
+Pixel gfx_getPixel(int r, int g, int b);
+void gfx_fillBuffer(Pixel color);
+
