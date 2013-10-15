7b613f847911b45487690ed0d4c4844311b2cc1b
Ohto Myllynen <ohromy@utu.fi>
2013-10-15 18:27:26 +0300

    Moved tracer specific declarations to tracer.h

--- declare.h
+++ declare.h
@@ -1,13 +1,3 @@
 #include "gfx.h"
 #include "tracer.h"
 
-void rt_initScene();
-void rt_printScene();
-void rt_setCameraPosition();
-void rt_setCameraDirection();
-void rt_setLightCount(int count);
-void rt_setObjectCount(int count);
-void rt_cleanup();
-void rt_setLight(int num, Light *light);
-void rt_setObject(int num, Object *obj);
-void rt_setResolution(int w, int h);
--- tracer.h
+++ tracer.h
@@ -58,3 +58,14 @@ typedef struct
 
 } Scene;
 
+void rt_initScene();
+void rt_printScene();
+void rt_setCameraPosition();
+void rt_setCameraDirection();
+void rt_setLightCount(int count);
+void rt_setObjectCount(int count);
+void rt_cleanup();
+void rt_setLight(int num, Light *light);
+void rt_setObject(int num, Object *obj);
+void rt_setResolution(int w, int h);
+
